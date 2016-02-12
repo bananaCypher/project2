e7950d27144e00a46d44b69e35bdbeccaee04b82
CallumDougan <practicalitymalfunction@gmail.com>
2016-02-12 14:02:47 +0000

    imported basic Investment model for further testing

--- client/src/models/specs/user_specs.js
+++ client/src/models/specs/user_specs.js
@@ -1,4 +1,5 @@
 var User = require('../user.js');
+var Investment = require('../Investment.js');
 
 var chai = require('chai')
 var assert = chai.assert;
@@ -7,7 +8,13 @@ var expect = chai.expect;
 describe('User', function(){
   beforeEach(function(){
     testUser = new User('Barry');
-
+    testInvestment = new Investment({
+      "name": 'BarryCorp',
+      "epic": "BRC",
+      "price": 210.75,
+      "quantity": 1000,
+      "buyPrice": 198.22,
+    });
   });
 
   it('should have a name', function(){
