c6e1648da80ac741239188fafdd2f02e9b765117
CallumDougan <practicalitymalfunction@gmail.com>
2016-02-12 13:48:38 +0000

    added, tested User.accountBalance

--- client/src/models/specs/user_specs.js
+++ client/src/models/specs/user_specs.js
@@ -1,13 +1,19 @@
 var User = require('../user.js');
 
-var assert = require('assert');
-var expect = require('expect');
+var chai = require('chai')
+var assert = chai.assert;
+var expect = chai.expect;
 
 describe('User', function(){
   beforeEach(function(){
-    var user = new User('Barry');
+    testUser = new User('Barry');
   });
+
   it('should have a name', function(){
-    expect(User.name).to.equal('Barry');
+    expect(testUser.name).to.equal('Barry');
   });
+
+  it('should have an account with money', function(){
+    expect(testUser.accountBalance).to.equal(500);
+  })
 })
--- client/src/models/user.js
+++ client/src/models/user.js
@@ -1,3 +1,7 @@
 var User = function(name){
-  this.name = name
+  this.name = name,
+  this.portfolio = null,
+  this.accountBalance = 500
 }
+
+module.exports = User;
