b81621ba408f737d6ad4475531f34ea932fe5722
Beth Fraser <bethanyfraser@live.co.uk>
2016-02-12 13:49:42 +0000

    Investments test and models made

---
+++ client/src/models/investment.js
@@ -0,0 +1,13 @@
+var Investment = function(params){
+  this.shareName = params.name;
+  this.epic = params.epic;
+  this.currentPrice = params.price;
+  this.quantity = params.quantity;
+  this.buyPrice = params.buyPrice;
+  this.pastCloseOfDayPrices = params.pastCloseOfDayPrices;
+  this.buyDate = params.buyDate;
+};
+
+
+
+module.exports = Investment;
