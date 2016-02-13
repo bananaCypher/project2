c94c14cecac1ba897bb0907769a1b89f3cec53de
Callum Hogg <bananaCypher@gmail.com>
2016-02-13 10:02:28 +0000

    Moved the express.static line in the server file
    There was an issue with the ordering of the 'app.use(express.static('client/build'))' line in the server file as pointed out here http://stackoverflow.com/questions/14150332/in-node-js-express-the-default-router-always-jump-to-index-html-static-file this was preventing the '/' route from running any javascript.

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path')
 
-app.use(express.static('client/build'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+app.use(express.static('client/build'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
