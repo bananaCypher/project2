446b015e218a20c9a258e58751c69077c53db514
CallumDougan <practicalitymalfunction@gmail.com>
2016-02-13 15:58:07 +0000

    User can now make ans settle short sells

--- client/src/models/user.js
+++ client/src/models/user.js
@@ -5,7 +5,7 @@ var User = function(name){
   this.portfolio = undefined,
   this.accountBalance = 500,
   this.insideTrader = false
-}
+};
 
 User.prototype = {
   buyShares: function(share, quantity, params){
@@ -20,8 +20,26 @@ User.prototype = {
     this.portfolio.removeInvestment(investment);
     this.accountBalance += outlay;
   },
+  sellShort: function(share, quantity, params){
+    var outlay = share.currentPrice * quantity;
+    var investment = new Investment(share, params);
+    investment.quantity = quantity;
+    investment.short = true;
+    this.portfolio.addInvestment(investment);
+    this.accountBalance += outlay;
+  },
+  buyShort: function(investment){
+    if(!investment.short){
+      console.log('this action is illegal!');
+    }
+    else{
+      var outlay = investment.share.currentPrice * investment.quantity;
+      this.portfolio.removeInvestment(investment);
+      this.accountBalance -= outlay;
+    }
+  },
   spreadRumours: function(investment, percentage){
-    if(this.insideTrader == false){
+    if(!this.insideTrader){
       console.log('this action is illegal!');
     }
     else{
