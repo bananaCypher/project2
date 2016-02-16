5056aa8a02b396b2192dcd4701608840b6d2dd44
Callum Hogg <bananaCypher@gmail.com>
2016-02-16 17:55:50 +0000

    Got index.js to display the form as needed

--- client/src/index.js
+++ client/src/index.js
@@ -105,10 +105,81 @@ var init = function(){
         errorList.appendChild(li);
         }
     );
-
+  // Target Form
   var targetsView = document.getElementById('targetsView');
   var targetsInfo = document.getElementById('targetsInfo');
   var targetFormButton = document.getElementById('targetFormButton');
+  var targetFormType = document.getElementById('targetFormType');
+  var targetFormFields = document.getElementById('targetFormFields');
+
+  var showFormForPortfolio = function(){
+    var label = document.getElementById('targetFormLabel'); 
+    var select = document.getElementById('targetFormObject');
+    var propSelect = document.getElementById('targetFormProp')
+    propSelect.innerHTML = '';
+    label.innerText = 'Portfolio';
+    var option = document.createElement('option');
+    option.innerText = 'My Portfolio';
+    select.appendChild(option);
+    var propOption = document.createElement('option');
+    propOption.innerText = 'Total value';
+    propOption.value = 'totalValue';
+    propSelect.appendChild(propOption);
+  }
+  var showFormForInvestment = function(){
+    var label = document.getElementById('targetFormLabel'); 
+    var select = document.getElementById('targetFormObject');
+    var propSelect = document.getElementById('targetFormProp')
+    propSelect.innerHTML = '';
+    label.innerText = 'Investment';
+    for (var investment of Barry.portfolio.investments) {
+      var option = document.createElement('option');
+      option.value = investment.shareName;
+      option.innerText = investment.shareName;
+      select.appendChild(option);
+    }
+    var propOption = document.createElement('option');
+    propOption.innerText = 'Current value';
+    propOption.value = 'currentValue';
+    propSelect.appendChild(propOption);
+    var propOption = document.createElement('option');
+    propOption.innerText = 'Seven day average';
+    propOption.value = 'sevenDayAverage';
+    propSelect.appendChild(propOption);
+  }
+  var showFormForShare = function(){
+    var label = document.getElementById('targetFormLabel'); 
+    var select = document.getElementById('targetFormObject');
+    var propSelect = document.getElementById('targetFormProp')
+    propSelect.innerHTML = '';
+    label.innerText = 'Share';
+    for (var investment of Barry.portfolio.investments) {
+      var option = document.createElement('option');
+      option.value = investment.share.shareName;
+      option.innerText = investment.share.shareName;
+      select.appendChild(option);
+    }
+    var propOption = document.createElement('option');
+    propOption.innerText = 'Current price';
+    propOption.value = 'currentPrice';
+    propSelect.appendChild(propOption);
+  }
+
+  targetFormType.onchange = function(){
+    if (this.value == 'Portfolio'){
+      targetFormFields.style.display = 'block';
+      showFormForPortfolio();
+    } else if (this.value == 'Investment'){
+      targetFormFields.style.display = 'block';
+      showFormForInvestment();
+    } else if (this.value == 'Share') {
+      targetFormFields.style.display = 'block';
+      showFormForShare();
+    } else {
+      targetFormFields.style.display = 'none';
+    }
+  }
+  // ==================
 
   Highcharts.setOptions(chartStyles);
 
