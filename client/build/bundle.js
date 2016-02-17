9742e6c3c73b6fcb52837d926fad14a380b03d53
Beth Fraser <bethanyfraser@live.co.uk>
2016-02-17 22:57:35 +0000

    Target value autofill working.

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -1764,8 +1764,16 @@
 	  var targetsView = document.getElementById('targetsView');
 	  targetsView.innerHTML = "";
 	
+	  var targetValue = "";
+	  var targets = user.findTargetsByShareName(investment.shareName);
+	  for(target of targets){
+	    if(target.property === "currentValue" && target.check === "gt"){
+	      targetValue = target.target / 100;
+	    }
+	  }
+	
 	  var p = document.createElement('p');
-	  p.innerHTML = "Target value for this investment (£): <input type='text' id='targetValue'><button id='targetValueButton'>Check</button><br>Price required to meet this target with current share quantity: <span id='targetValuePrice'></span><br><br>Days to hit target if current trend continues: <span id='targetValueDays'></span>";
+	  p.innerHTML = "Target value for this investment (£): <input type='text' id='targetValue' value='" + targetValue + "''><button id='targetValueButton'>Check</button><br>Price required to meet this target with current share quantity: <span id='targetValuePrice'></span><br><br>Days to hit target if current trend continues: <span id='targetValueDays'></span>";
 	
 	  targetsView.appendChild(p);
 	
@@ -1788,7 +1796,6 @@
 	      else {
 	        var difference = parseInt(input) - investment.currentValue();
 	        var averageIncrease = (investment.currentValue() - (investment.share.pastCloseOfDayPrices[0] * investment.quantity)) / 8;
-	        console.log(investment.share.pastCloseOfDayPrices[0] * investment.quantity)
 	        var days = difference / averageIncrease;
 	        if(days < 0){
 	          return "Investment value is currently decreasing."
--- client/src/targets.js
+++ client/src/targets.js
@@ -2,8 +2,16 @@ var TargetChecker = function(user, investment){
   var targetsView = document.getElementById('targetsView');
   targetsView.innerHTML = "";
 
+  var targetValue = "";
+  var targets = user.findTargetsByShareName(investment.shareName);
+  for(target of targets){
+    if(target.property === "currentValue" && target.check === "gt"){
+      targetValue = target.target / 100;
+    }
+  }
+
   var p = document.createElement('p');
-  p.innerHTML = "Target value for this investment (£): <input type='text' id='targetValue'><button id='targetValueButton'>Check</button><br>Price required to meet this target with current share quantity: <span id='targetValuePrice'></span><br><br>Days to hit target if current trend continues: <span id='targetValueDays'></span>";
+  p.innerHTML = "Target value for this investment (£): <input type='text' id='targetValue' value='" + targetValue + "''><button id='targetValueButton'>Check</button><br>Price required to meet this target with current share quantity: <span id='targetValuePrice'></span><br><br>Days to hit target if current trend continues: <span id='targetValueDays'></span>";
 
   targetsView.appendChild(p);
 
@@ -26,7 +34,6 @@ var TargetChecker = function(user, investment){
       else {
         var difference = parseInt(input) - investment.currentValue();
         var averageIncrease = (investment.currentValue() - (investment.share.pastCloseOfDayPrices[0] * investment.quantity)) / 8;
-        console.log(investment.share.pastCloseOfDayPrices[0] * investment.quantity)
         var days = difference / averageIncrease;
         if(days < 0){
           return "Investment value is currently decreasing."
